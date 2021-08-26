24d2f713e264f06137a6a4449982aadb0ab3e494
Tony Bowden <tony@tmtm.com>
2021-08-26 17:35:16 +0300

    remove rows that differ only in Labels
    we want to include the labels for display, but not report on anything
    where the only diff is in the label itself — i.e. if the Qids are the
    same, it doesn't matter what the items are called.

--- bin/diff.rb
+++ bin/diff.rb
@@ -6,5 +6,9 @@ require 'pry'
 
 diff = EveryPoliticianScraper::Comparison.new('data/wikidata.csv', 'data/wikipedia.csv').diff
 csv = CSV.parse(diff.map(&:to_csv).join, headers:true)
+         .delete_if do |row|
+            row['@@'] == '->' && 
+              (row.select { |k, v| v.to_s.include?('->') }.map(&:first) - ['@@', 'country', 'positionlabel', 'officeholderlabel']).none?
+        end
 puts csv.headers.to_csv
 puts csv.sort_by { |row| [row[1],row[3],row[6],row[4]] }.map(&:to_s)
