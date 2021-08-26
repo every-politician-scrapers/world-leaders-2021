29559c9aaa8a53e396ab43d0bfafbcb8af5210c2
Tony Bowden <tony@tmtm.com>
2021-08-26 17:29:16 +0300

    Treat diff as CSV
    Precursor to adding more logic to this

--- bin/diff.rb
+++ bin/diff.rb
@@ -2,6 +2,9 @@
 # frozen_string_literal: true
 
 require 'every_politician_scraper/comparison'
+require 'pry'
 
-header, *diffs = EveryPoliticianScraper::Comparison.new('data/wikidata.csv', 'data/wikipedia.csv').diff
-puts header.to_csv, diffs.sort_by { |r| [r[1].to_s] }.map(&:to_csv)
+diff = EveryPoliticianScraper::Comparison.new('data/wikidata.csv', 'data/wikipedia.csv').diff
+csv = CSV.parse(diff.map(&:to_csv).join, headers:true)
+puts csv.headers.to_csv
+puts csv.sort_by { |row| [row[1],row[3],row[6],row[4]] }.map(&:to_s)
