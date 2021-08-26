f86a60185eb89a2a7d8c200b0e22ad40b05087a9
Tony Bowden <tony@tmtm.com>
2021-08-26 16:37:07 +0300

    Reformat the Wikidata output
    This will create more false diffs purely based on name variations, but
    it'll make all the diffs much easier to follow.

--- bin/scraper/wikidata.rb
+++ bin/scraper/wikidata.rb
@@ -4,9 +4,9 @@
 require 'every_politician_scraper/wikidata_query'
 
 query = <<SPARQL
-  SELECT DISTINCT (STRAFTER(STR(?countryItem), STR(wd:)) AS ?country) ?countryItemLabel
-                  (STRAFTER(STR(?positionItem), STR(wd:)) AS ?position) ?positionItemLabel
-                  (STRAFTER(STR(?officeholderItem), STR(wd:)) AS ?officeholder) ?officeholderItemLabel
+  SELECT DISTINCT (STRAFTER(STR(?countryItem), STR(wd:)) AS ?countryid) ?country
+                  (STRAFTER(STR(?positionItem), STR(wd:)) AS ?position) ?positionLabel
+                  (STRAFTER(STR(?officeholderItem), STR(wd:)) AS ?officeholder) ?officeholderLabel
     (YEAR(?start) AS ?start_year) (YEAR(?end) AS ?end_year)
   WHERE {
     ?countryItem wdt:P31 wd:Q3624078 ; p:P1313|p:P1906 ?os .
@@ -18,9 +18,14 @@ query = <<SPARQL
     OPTIONAL { ?ps pq:P582 ?end }
     FILTER(!BOUND(?end) || YEAR(?end)=2021)
 
-    SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+    SERVICE wikibase:label {
+      bd:serviceParam wikibase:language "en".
+      ?countryItem rdfs:label ?country.
+      ?positionItem rdfs:label ?positionLabel .
+      ?officeholderItem rdfs:label ?officeholderLabel .
+    }
   }
-  ORDER BY ?countryItemLabel ?positionLabel ?startYear
+  ORDER BY ?country ?positionLabel ?startYear
 SPARQL
 
 agent = 'every-politican-scrapers/world=leaders-2021'
